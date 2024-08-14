c803e62369295d48e320a9a7a43b11f8c23b1453
JeanPerez29 <163458132+JeanPerez29@users.noreply.github.com>
2024-08-13 20:55:22 -0500

    Add files via upload

---
+++ MODELOS/JeanPerez/modelos/tienda/productos/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Producto
+
+class ProductoModelForm(forms.ModelForm):
+    class Meta:
+        model = Producto
+        fields = ['nombre', 'descripcion', 'precio', 'stock']
+        
---
+++ MODELOS/JeanPerez/modelos/tienda/productos/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from .models import Cliente
+from .models import Producto
+from .forms import ProductoModelForm
+
+# Create your views here.
+
+def listar_clientes(request):
+    clientes = Cliente.objects.all()
+    return render(request, 'listar_clientes.html', {'clientes': clientes})
+
+def listar_productos(request):
+    productos = Producto.objects.all()
+    return render(request, 'listar_productos.html', {'productos': productos})
+
+def inicio(request):
+    return render(request, 'inicio.html')
+
+def agregar_producto(request):
+    if request.method == 'POST':
+        form = ProductoModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_productos')
+    else:
+        form = ProductoModelForm
+        return render(request, 'agregar_producto.html', {'form': form})
